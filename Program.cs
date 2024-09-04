1d9ca5ace1db58b40c7462c0ab3b8eae0d6b9583
mkheck13 <mkheck13@gmail.com>
2024-09-04 15:47:44 -0700

    Dayone C#

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// Two forward slashes are used to comment on a single line of code
+
+/*
+This is a way to comment out multiple lines of code
+line1
+line2
+*/
+
+// Console.Clear() will clear the console screen
+Console.Clear();
+
+// Console.WriteLine() will print any value that is in between the paranthesis and add the value to a new line
+Console.WriteLine("Hello, World!");
+
+// Console.Write() will print out any value and keep the text/cursor on the same line
+Console.Write("Michael Heckerman" + " ");
+Console.Write("Likes Taco Bell");
+Console.WriteLine("");
+Console.WriteLine("I am new line");
+Console.WriteLine("");
+Console.WriteLine("<------------New Section------------>"); 
+Console.WriteLine("");
+
+Console.Write("T");
+Console.Write("a");
+Console.Write("c");
+Console.Write("o");
+Console.WriteLine("");
+Console.WriteLine("<------------New Section------------>"); 
+Console.WriteLine("");
+
+Console.WriteLine("What is your name?");
+
+string myName = Console.ReadLine();
+Console.WriteLine("Hello " + myName);
+// Console.WriteLine(myName);
+
+
+
+// Console.ReadLine() allows you to enter into the terminal
+// Console.ReadLine();
